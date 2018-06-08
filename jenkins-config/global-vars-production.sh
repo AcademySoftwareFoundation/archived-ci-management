a6929ecb7e37254b68747e6fe6e5c09daf3e927a
Thanh Ha <thanh.ha@linuxfoundation.org>
2018-06-08 14:41:35 -0400

    Setup global-vars management
    Add configuration for jenkins-cfg job to manage global-vars.
    
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

---
+++ jenkins-config/global-vars-production.sh
@@ -0,0 +1,6 @@
+GIT_URL=https://github.com
+GIT_CLONE_URL=git@github.com:
+JENKINS_HOSTNAME=vex-yul-mpsf-jenkins-prod-1
+NEXUS_URL=https://nexus.motionpicturesoftwarefoundation.org/
+SILO=production
+SONAR_URL=https://sonar.opendaylight.org
