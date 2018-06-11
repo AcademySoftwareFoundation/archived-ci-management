ca495a111cd08877c67ebf824fae2e5de744ca71
Thanh Ha <thanh.ha@linuxfoundation.org>
2018-06-11 15:55:11 -0400

    Fix sandbox global-vars
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- jenkins-config/global-vars-sandbox.sh
+++ jenkins-config/global-vars-sandbox.sh
@@ -1,6 +1,6 @@
 GIT_URL=https://github.com
 GIT_CLONE_URL=git@github.com:
-JENKINS_HOSTNAME=vex-yul-mpsf-jenkins-prod-1
+JENKINS_HOSTNAME=vex-yul-mpsf-jenkins-sandbox-1
 NEXUS_URL=https://nexus.motionpicturesoftwarefoundation.org/
-SILO=production
+SILO=sandbox
 SONAR_URL=https://sonar.opendaylight.org
