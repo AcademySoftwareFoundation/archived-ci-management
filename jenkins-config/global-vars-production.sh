ce1803671dbde1d507208ccd1ae3d74e5ea5fa23
Thanh Ha <thanh.ha@linuxfoundation.org>
2018-06-18 17:21:46 -0400

    Add log server configuration
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- jenkins-config/global-vars-production.sh
+++ jenkins-config/global-vars-production.sh
@@ -1,6 +1,7 @@
 GIT_URL=https://github.com
 GIT_CLONE_URL=git@github.com:
 JENKINS_HOSTNAME=vex-yul-mpsf-jenkins-prod-1
+LOGS_SERVER=https://logs.motionpicturesoftwarefoundation.org/
 NEXUS_URL=https://nexus.motionpicturesoftwarefoundation.org/
 SILO=production
 SONAR_URL=https://sonar.opendaylight.org
--- jenkins-config/global-vars-sandbox.sh
+++ jenkins-config/global-vars-sandbox.sh
@@ -1,6 +1,7 @@
 GIT_URL=https://github.com
 GIT_CLONE_URL=git@github.com:
 JENKINS_HOSTNAME=vex-yul-mpsf-jenkins-sandbox-1
+LOGS_SERVER=https://logs.motionpicturesoftwarefoundation.org/
 NEXUS_URL=https://nexus.motionpicturesoftwarefoundation.org/
 SILO=sandbox
 SONAR_URL=https://sonar.opendaylight.org
