5bcae07d2680eb20d24b06aa29e080e06a91269d
Thanh Ha <thanh.ha@linuxfoundation.org>
2018-07-16 16:18:01 -0400

    Update variables for aswf.io
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- jenkins-config/global-vars-production.sh
+++ jenkins-config/global-vars-production.sh
@@ -1,7 +1,7 @@
 GIT_URL=https://github.com
 GIT_CLONE_URL=git@github.com:
 JENKINS_HOSTNAME=vex-yul-mpsf-jenkins-prod-1
-LOGS_SERVER=https://logs.motionpicturesoftwarefoundation.org/
-NEXUS_URL=https://nexus.motionpicturesoftwarefoundation.org/
+LOGS_SERVER=https://logs.aswf.io/
+NEXUS_URL=https://nexus.aswf.io/
 SILO=production
-SONAR_URL=https://sonar.opendaylight.org
+SONAR_URL=https://sonar.aswf.io
