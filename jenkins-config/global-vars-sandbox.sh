dccdf85bc9c9a1a697f946049c1d2fdfda8b35a6
Thanh Ha <thanh.ha@linuxfoundation.org>
2018-07-19 21:38:30 -0400

    Remove trailing / from URLs
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- jenkins-config/global-vars-sandbox.sh
+++ jenkins-config/global-vars-sandbox.sh
@@ -1,7 +1,7 @@
 GIT_URL=https://github.com
 GIT_CLONE_URL=git@github.com:
 JENKINS_HOSTNAME=vex-yul-mpsf-jenkins-sandbox-1
-LOGS_SERVER=https://logs.aswf.io/
-NEXUS_URL=https://nexus.aswf.io/
+LOGS_SERVER=https://logs.aswf.io
+NEXUS_URL=https://nexus.aswf.io
 SILO=sandbox
 SONAR_URL=https://sonar.aswf.io
