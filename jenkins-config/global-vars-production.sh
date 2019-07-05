ad1ec039727edc3e74626d1ab42c41c843f5fc96
Jessica Wagantall <jwagantall@linuxfoundation.org>
2019-07-05 12:47:57 -0700

    Add HOUPASS to the environment
    Add HOUPASS var in both production and
    sandbox configurations.
    
    Issue: IT-16687
    Signed-off-by: Jessica Wagantall <jwagantall@linuxfoundation.org>

--- jenkins-config/global-vars-production.sh
+++ jenkins-config/global-vars-production.sh
@@ -1,5 +1,6 @@
 GIT_URL=https://github.com
 GIT_CLONE_URL=git@github.com:
+HOUPASS=L3_M2f2W
 JENKINS_HOSTNAME=vex-yul-mpsf-jenkins-prod-1
 LOGS_SERVER=https://logs.aswf.io
 NEXUS_URL=https://nexus.aswf.io
